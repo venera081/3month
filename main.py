6f3e0b6dc89e1b4a0094f4c909c271427cf6d603
venera081 <berekettop0010@gmail.com>
2025-09-08 11:48:31 +0600

    lesson 3

--- main.py
+++ main.py
@@ -4,23 +4,36 @@ def main(page: ft.Page):
     page.title = "Моё первое приложение!"
     page.theme_mode = ft.ThemeMode.DARK
 
+
     greeting_text = ft.Text(value="Hello world!")
-    name_input = ft.TextField(label="Введите имя: ")
+
+    greeting_history = []
+    history_text = ft.Text('История приветствий:')
 
     def on_button_click(e):
         name = name_input.value.strip()
-        print(name)
+        #print(name)
 
-        greeting_text.value = f"Hello {name}"
-        print(greeting_text)
+        if name:
+            greeting_text.value = f"Hello {name}!"
+            #print(greeting_text)
+            name_input.value = ''
+
+            greeting_history.append(f"{name}")
+            history_text.value = "История приветствий:\n " + "\n".join(greeting_history)
+
+        else:
+            greeting_text.value = "Пожалуйста, введите имя"
 
         page.update()
 
+    name_input = ft.TextField(label="Введите имя: ", on_submit=on_button_click)
     name_button = ft.ElevatedButton(text="SEND", on_click=on_button_click)
 
 
-    page.add(greeting_text, name_input, name_button)
+    page.add(greeting_text, name_input, name_button, history_text)
 
 
 ft.app(target=main)    
+
 
